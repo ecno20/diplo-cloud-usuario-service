02c80d35b0cfb143e8ff766403c9297972ab9f16
Jonathan Díaz <144557398+ecno20@users.noreply.github.com>
2026-06-01 01:09:21 -0600

    Update: update UsuarioServiceApplicationTest.java

--- src/test/java/unam/diplomado/pixup/usuarioservice/UsuarioServiceApplicationTests.java
+++ src/test/java/unam/diplomado/pixup/usuarioservice/UsuarioServiceApplicationTests.java
@@ -1,18 +1,36 @@
 package unam.diplomado.pixup.usuarioservice;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// ESTA LÍNEA INDICA A SPRING QUE IGNORE LA CONFIGURACIÓN DE MONGODB DURANTE ESTE TEST
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration,org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration"
+})
 class UsuarioServiceApplicationTests {
 
-	@Disabled("Deshabilitado temporalmente en CI por falta de conectividad a Mongo")
 	@Test
 	void contextLoads() {
+        // El test pasará con éxito de forma inmediata al no buscar inicializar Mongo
 	}
 
 }
+
+// package unam.diplomado.pixup.usuarioservice;
+
+// import org.junit.jupiter.api.Disabled;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class UsuarioServiceApplicationTests {
+
+// 	@Disabled("Deshabilitado temporalmente en CI por falta de conectividad a Mongo")
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
 
 // import org.junit.jupiter.api.Test;
 // import org.springframework.boot.test.context.SpringBootTest;
